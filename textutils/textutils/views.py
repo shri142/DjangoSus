97cb6a3fca57aca3b7d7cb0519fcc8d891193632
shri142 <shrikrishna2002umbare@gmail.com>
2022-07-02 23:48:07 +0530

    Update urls.py

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,5 +1,32 @@
 # I have creaated this file
 
 from django.http import HttpResponse
+# def index(request):
+#     return HttpResponse("hello")
+#
+# def about(request):
+#     return HttpResponse("hello in about")
+#
+# def joke(request):
+#     file=open("fun.txt","r+")
+#     data=file.read()
+#     return HttpResponse(data)
+
 def index(request):
-    return HttpResponse("hello")
+    return HttpResponse("Home")
+
+def removepunc(request):
+    return HttpResponse("remove punc")
+
+def capfirst(request):
+    return HttpResponse("capitalize first")
+
+def newlineremove(request):
+    return HttpResponse("capitalize first")
+
+
+def spaceremove(request):
+    return HttpResponse("space remover")
+
+def charcount(request):
+    return HttpResponse("charcount ")
