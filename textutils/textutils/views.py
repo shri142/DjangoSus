4f26395e87ea5e1874c23a429b8d374a490662fa
shri142 <shrikrishna2002umbare@gmail.com>
2022-07-03 00:00:08 +0530

    Added templates

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,8 +1,15 @@
 # I have creaated this file
 
 from django.http import HttpResponse
-# def index(request):
-#     return HttpResponse("hello")
+from django.shortcuts import render
+
+
+
+
+
+
+def index(request):
+    return render(request,'index.html')
 #
 # def about(request):
 #     return HttpResponse("hello in about")
@@ -12,21 +19,21 @@ from django.http import HttpResponse
 #     data=file.read()
 #     return HttpResponse(data)
 
-def index(request):
-    return HttpResponse("Home")
+# def index(request):
+#     return HttpResponse("Home")
 
-def removepunc(request):
-    return HttpResponse("remove punc")
-
-def capfirst(request):
-    return HttpResponse("capitalize first")
-
-def newlineremove(request):
-    return HttpResponse("capitalize first")
-
-
-def spaceremove(request):
-    return HttpResponse("space remover")
-
-def charcount(request):
-    return HttpResponse("charcount ")
+# def removepunc(request):
+#     return HttpResponse("remove punc")
+#
+# def capfirst(request):
+#     return HttpResponse("capitalize first")
+#
+# def newlineremove(request):
+#     return HttpResponse("capitalize first")
+#
+#
+# def spaceremove(request):
+#     return HttpResponse("space remover")
+#
+# def charcount(request):
+#     return HttpResponse("charcount ")
