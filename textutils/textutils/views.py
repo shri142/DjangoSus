aaedfe7ae07dafbd2a40042c4f041c5c3859156b
shri142 <shrikrishna2002umbare@gmail.com>
2022-07-04 13:44:05 +0530

    Update index.html

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -10,6 +10,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def analyze(request):
+    # Get the text
+    djtext = request.GET.get('text', 'default')
+    removepunc=request.GET.get('removepunc','off')
+
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    else:
+        return HttpResponse('Error')
 #
 # def about(request):
 #     return HttpResponse("hello in about")
